293c803d8b813fe45819edef98177d8565080695
Reena <reenahathaliya411@gmail.com>
2021-04-30 13:15:56 +0530

    linked added

--- my-app/src/Pages/Home.jsx
+++ my-app/src/Pages/Home.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
-import { NavLink } from 'react-router-dom'
-=======
 import { Link } from 'react-router-dom'
->>>>>>> 17715136ea41c5a9464903efc987f30093fef9a3
 
 const Home = () => {
     return (
@@ -13,11 +9,7 @@ const Home = () => {
                 <div className="banner-detail">
                     <h1>Hi there! We are the new kids on the block <br/>
                     and we build awesome websites and mobile apps.</h1>
-<<<<<<< HEAD
-                    <NavLink to="/" className="btn-orange">WORK WITH US!</NavLink>
-=======
                     <Link to="/" className="btn-orange">WORK WITH US!</Link>
->>>>>>> 17715136ea41c5a9464903efc987f30093fef9a3
                 </div>
             </div>
         </div>
